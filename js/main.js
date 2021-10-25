cc866cac5d7c35d48c8ffc6d914755bcf941af4b
RuslanBakhtiyarov <bakhtiyarovruslan1@gmail.com>
2021-10-25 14:27:39 +0300

    4

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button-prev',
+    prevEl: '.slider-button-next',
+  },
+
+});
